474baa990f8f62bc7bd59fdfaac8d403b45977d1
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-18 22:47:53 -0600

    Simplified testing

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,20 +1,4 @@
-const puppeteer = require("puppeteer")
-
 describe("First example test", () => {
-	let browser, page
-
-	beforeAll(async () => {
-		browser = await puppeteer.launch({
-			defaultViewport: null
-		})
-
-		page = await browser.newPage()
-	})
-
-	afterAll(async () => {
-		await browser.close()
-	})
-
 	it("Navigating to a page", async () => {
 		await page.goto("https://google.com")
 
