b06462cd386f8a6d19236670d528bcc5805da553
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-19 23:05:05 -0600

    Reporter added, but report is created on test

--- jest.config.js
+++ jest.config.js
@@ -106,7 +106,15 @@ module.exports = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+		"default",
+		[
+			"./node_modules/jest-html-reporter",
+			{
+				pageTitle: "Testing report"
+			}
+		]
+	],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
